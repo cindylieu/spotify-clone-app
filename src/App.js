71e3c2e907b882a7a1f93c1bc33eff40481ca9e8
Cindy Lieu <Cindy Lieu>
2021-07-15 12:53:41 -0400

    set up sidebar, added playlists to sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
-import Login from './Login';
+import "./App.css";
+import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from './Player';
+import Player from "./Player";
 import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
@@ -18,15 +18,22 @@ function App() {
 
     if (_token) {
       dispatch({
-        type: 'SET_TOKEN',
-        token: _token
-      })
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
         dispatch({
-          type: 'SET_USER',
-          user: user
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
         });
       });
     }
@@ -34,13 +41,7 @@ function App() {
 
   return (
     <div className="app">
-      {
-        token ? (
-          <Player spotify={spotify} />
-        ) : (
-          <Login />
-        )
-      }
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,32 +1,38 @@
 export const initialState = {
-    user: null,
-    playlists: [],
-    playing: false,
-    item: null,
-    token: null,
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  token: null,
 };
 
 const reducer = (state, action) => {
-    console.log(action);
+  console.log(action);
 
-    // action -> type, [payload]
-    // listening for action
-    switch (action.type) {
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.user
-            };
+  // action -> type, [payload]
+  // listening for action
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
 
-        case 'SET_TOKEN':
-            return {
-                ...state,
-                token: action.token
-            }
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
 
-        default:
-            return state;
-    }
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
